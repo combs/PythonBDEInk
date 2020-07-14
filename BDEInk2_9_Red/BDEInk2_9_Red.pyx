b907e11a66d5fdd25e19d61f68448e4fc3e59055
Chris Combs <ccombs@gmail.com>
2020-07-14 12:38:55 -0400

    wrong func

--- BDEInk2_9_Red/BDEInk2_9_Red.pyx
+++ BDEInk2_9_Red/BDEInk2_9_Red.pyx
@@ -4,7 +4,7 @@ cimport c_029_1R
 cdef class BDEInk2_9_Red(object):
     cpdef start(self):
         print("hi")
-        c_029_1R.EPD_Init()
+        c_029_1R.DEV_ModuleInit() 
         # print(mod.EPD_Init())
         return
     cpdef render(self,image1,image2):
--- c_029_1R/c_029_1R.pxd
+++ c_029_1R/c_029_1R.pxd
@@ -4,4 +4,4 @@
 ctypedef int uint8_t
 
 cdef extern from "../libraries/eastrising/029_1R/bcm2835/obj/ER-EPD029-1R.h":
-    uint8_t EPD_Init()
+    uint8_t DEV_ModuleInit()
