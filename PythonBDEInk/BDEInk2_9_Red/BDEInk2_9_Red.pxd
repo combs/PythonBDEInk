2a329521729b7ccd07a58b7f03faa78ad001705e
Chris Combs <ccombs@gmail.com>
2020-07-15 17:32:24 -0400

    bump ver

--- PythonBDEInk/BDEInk2_9_Red/BDEInk2_9_Red.pxd
+++ PythonBDEInk/BDEInk2_9_Red/BDEInk2_9_Red.pxd
@@ -1,6 +1,8 @@
 
 cdef class BDEInk2_9_Red (object):
-    cdef public bint deviceinit
+
+    cdef public bint deviceinit 
+
     cpdef start(self)
     cpdef clear(self)
     cpdef render(self,image1,image2)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ PythonBDEInk = Extension(
 
 setup(
     name="PythonBDEInk",
-    version='1.0',
+    version='1.1',
     author='Chris Combs',
     author_email='ccombs@gmail.com',
     ext_modules=cythonize([PythonBDEInk])
